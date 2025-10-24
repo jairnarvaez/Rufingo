29a0c90845bf2fac47270bce7de0cfed0729f8fd
jairnarvaez <jairandresnar@gmail.com>
2025-10-23 22:57:02 -0500

    fix: desactivar cache de las paginas dinamicas, middleware

--- config/settings.py
+++ config/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'flashcards.middleware.NoCacheMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
---
+++ flashcards/middleware.py
@@ -0,0 +1,14 @@
+class NoCacheMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        
+        # Solo aplicar a páginas HTML (no a static files)
+        if response.get('Content-Type', '').startswith('text/html'):
+            response['Cache-Control'] = 'no-cache, no-store, must-revalidate, max-age=0'
+            response['Pragma'] = 'no-cache'
+            response['Expires'] = '0'
+        
+        return response
